ae2d5918e15472d280214ab185c31c7b302954dd
Lokesh Meesala <meesala.lokesh@gmail.com>
2020-01-22 13:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pickle
 import pandas as pd
 import nltk
 import re
-import nltk
 nltk.download('stopwords')
+from nltk.corpus import stopwords
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
